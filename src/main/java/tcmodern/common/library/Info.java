9f4b054f312e4d3f6a84a4aeb9232d8f1d2656ed
broscolotos <broscolotos@gmail.com>
2025-12-13 21:32:16 -0600

    version bump

--- src/main/java/tcmodern/common/library/Info.java
+++ src/main/java/tcmodern/common/library/Info.java
@@ -3,7 +3,7 @@ package tcmodern.common.library;
 public class Info {
     public static final String modID = "tcmodern";
     public static final String modName = "TCModern Addon";
-    public static final String modVersion = "0.1.0";
+    public static final String modVersion = "0.1.3";
 
     public static final String trainsPrefix = modID + ":textures/trains/";
 }
